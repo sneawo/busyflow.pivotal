70f93130156d63a382e69d90fa724180ad1085c8
Andrey Zhukov <sneawo@gmail.com>
2013-06-05 16:45:29 +0200

    Fixed xml generation with xmlbuilder 1.0.

--- src/busyflow/pivotal/__init__.py
+++ src/busyflow/pivotal/__init__.py
@@ -172,14 +172,13 @@ class ProjectEndpoint(Endpoint):
                          newer_than_version=newer_than_version)
 
     def make_post_xml(self, name, iteration_length, point_scale):
-        project = XMLBuilder(format=True)
-        with project.project:
-            if name is not None:
-                project << ('name', name)
-            if iteration_length is not None:
-                project << ('iteration_length', iteration_length)
-            if point_scale is not None:
-                project << ('point_scale', point_scale)
+        project = XMLBuilder('project')
+        if name is not None:
+            project.name << name
+        if iteration_length is not None:
+            project.iteration_length << iteration_length
+        if point_scale is not None:
+            project.point_scale << point_scale
         return str(project)
 
     def post(self, name, iteration_length, point_scale):
@@ -226,36 +225,34 @@ class StoryEndpoint(Endpoint):
 
     def make_story_xml(self, name=None, description=None, story_type=None,
                        owned_by=None, requested_by=None, estimate=None, current_state=None, labels=None):
-        story = XMLBuilder(format=True)
-        with story.story:
-            if name is not None:
-                story << ('name', name)
-            if description is not None:
-                story << ('description', description)
-            if requested_by is not None:
-                story << ('requested_by', requested_by)
-            if owned_by is not None:
-                story << ('owned_by', owned_by)
-            if story_type is not None:
-                story << ('story_type', story_type)
-            if estimate is not None:
-                story << ('estimate', str(estimate), {'type': 'integer'})
-            if current_state is not None:
-                story << ('current_state', current_state)
-            if labels is not None:
-                label_string = ','
-                if labels:
-                    label_string = ','.join(labels)
-                story << ('labels', label_string)
+        story = XMLBuilder('story')
+        if name is not None:
+            story.name << name
+        if description is not None:
+            story.description << description
+        if requested_by is not None:
+            story.requested_by << requested_by
+        if owned_by is not None:
+            story.owned_by << owned_by
+        if story_type is not None:
+            story.story_type << story_type
+        if estimate is not None:
+            story.estimate << str(estimate)
+        if current_state is not None:
+            story.current_state << current_state
+        if labels is not None:
+            label_string = ','
+            if labels:
+                label_string = ','.join(labels)
+            story.labels << label_string
 
         return str(story)
 
     def make_comment_xml(self, text, author):
-        x = XMLBuilder(format=True)
-        with x.note:
-            x << ('text', text)
-            if author is not None:
-                x << ('author', author)
+        x = XMLBuilder('note')
+        x.text << text
+        if author is not None:
+            x.author << author
         return str(x)
 
     def all(self, project_id, query=None, limit=None, offset=None):
